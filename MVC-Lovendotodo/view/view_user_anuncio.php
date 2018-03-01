fabe1fd6214c27feef3606a0e01471a3504d2b5c
Zan <Zan@ZanPC>
2018-03-01 11:59:02 +0100

    Adición extra para el borrado

--- MVC-Lovendotodo/view/view_user_anuncio.php
+++ MVC-Lovendotodo/view/view_user_anuncio.php
@@ -7,9 +7,17 @@
             $n_reg=1;
             foreach ($datos as $dato) {
             echo   '<div class="panel panel-success">
-                        <div class="panel-heading"><IMG src="../resources/arrows/arrow-right.png" width="30" height="30" data-toggle="collapse" data-target="#demo'.$n_reg.'">
-                            <b>'.$dato["titulo"].'</b>'.' ||<b> Código : '.$dato["cod_anuncio"].' ||<b>   Propietario : </b>'.$dato["nom"].' '.$dato["ape"].'<b> TLF : </b>'.$dato["tlf"].'</div>
-                            <div class="row">
+                        <div class="panel-heading"><IMG src="../resources/arrows/arrow-right.png" width="30" height="30" data-toggle="collapse" data-target="#demo'.$n_reg.'">';
+            if (isset($_SESSION["usuario"])) 
+            {
+                 echo '<b>'.$dato["titulo"].'</b>'.' ||<b> Código : '.$dato["cod_anuncio"].' ||<b>   Propietario : </b>'.$dato["nom"].' '.$dato["ape"].'<b> TLF : </b>'.$dato["tlf"].'<a href="view-elim.php?pro=<?php echo $aux['.$dato["cod_anuncio"].']?>"><IMG src="../resources/ad/ad-del.png" width="30" height="30"></a></div>';
+            }   
+            else
+            {
+                 echo '<b>'.$dato["titulo"].'</b>'.' ||<b> Código : '.$dato["cod_anuncio"].' ||<b>   Propietario : </b>'.$dato["nom"].' '.$dato["ape"].'<b> TLF : </b>'.$dato["tlf"];
+            }
+           
+            echo                '<div class="row">
                                 <div id="demo'.$n_reg.'" class="collapse">
                                     <div class="col-md-12">
                                         <div class="panel-body"><b>Precio : </b> <span class="label label-primary">'.$dato["prec_prod"].' ||<b> Precio de venta:</b> </b> <span class="label label-primary">'.$dato["prec_venta"].' ||<b> Porcentaje :</b> </b> <span class="label label-primary">'.$dato["porcentaje"].'</span></div>
